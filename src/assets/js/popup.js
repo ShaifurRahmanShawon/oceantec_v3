69f3008aea0f1ea4b21ac12b0c74779321fea915
ShaifurRahmanShawon <shaifur999@gmail.com>
2024-04-10 21:34:22 +0600

    contact and portfolio

---
+++ src/assets/js/popup.js
@@ -0,0 +1,11 @@
+// JavaScript to control popup window
+
+// Function to open the popup
+function openPopup() {
+  document.getElementById("popup").style.display = "block";
+}
+
+// Function to close the popup
+function closePopup() {
+  document.getElementById("popup").style.display = "none";
+}
